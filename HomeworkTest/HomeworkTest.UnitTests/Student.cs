9ad8027f080f09c3def6ccbc441878f157aed44a
ytycmx <44239504+ytycmx@users.noreply.github.com>
2018-10-22 11:27:27 +0100

    As the step said, we need to replace all the NotImplementedException clauses in Student.cs with implementation code until all tests pass. So we decided to input all the real identities into three class properties, and used a structure Student assess the properties. Thus we got the final result.

---
+++ HomeworkTest/HomeworkTest.UnitTests/Student.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace HomeworkTest
+{
+    public class Student : IStudent
+    {
+        public string Name => "Zihao";
+
+        public string Surname => "Song";
+
+        public int MetriculationNumber => 1804548;
+
+        public Student(string name, string surname, int metriculationNumber)
+        {
+            name = Name;
+            surname = Surname;
+            metriculationNumber = MetriculationNumber;
+        } 
+    }
+}
+//name : Zihao Song
